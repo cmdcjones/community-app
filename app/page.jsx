a0f1aca54598d69b5c5c692ac6e9dd70ee954f49
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-02 16:20:51 -0400

    update Link hrefs

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ export default function Home() {
     return (
         <>
             <h1>Hello, Vibrance</h1>
-            <h2><Link href="/login">Log In</Link></h2>
-            <h2><Link href="/signup">Sign Up</Link></h2>
+            <h2><Link href="/auth/login">Log In</Link></h2>
+            <h2><Link href="/auth/signup">Sign Up</Link></h2>
         </>
     );
 }
