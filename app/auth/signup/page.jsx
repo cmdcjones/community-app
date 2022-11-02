3181da950c616745b353a722c91879897552f136
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-02 17:51:03 -0400

    add useState, state variables, form

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,9 +1,29 @@
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function SignUp() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        console.log("Default action prevented");
+    }
+
     return (
         <div className='signup-container'>
             <h1>Hello from SignUp</h1>
+            <div className='form-container'>
+                <form 
+                    method='POST'
+                    onSubmit={handleSubmit}
+                >
+                    <button>Submit me!</button>
+                </form>
+            </div>
             <Link href="/">Head back home</Link>
         </div>
     );
