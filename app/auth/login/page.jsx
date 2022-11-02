be27c6b7b29d2a9df613a6b1b55c3efbb57d3d94
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-02 16:02:39 -0400

    add login, signup

---
+++ app/auth/login/page.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function LogIn() {
+    return (
+        <div className='login-container'>
+            <h1>Hello from LogIn</h1>
+            <Link href="/">Head back home</Link>
+        </div>
+    );
+}
---
+++ app/auth/signup/page.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function SignUp() {
+    return (
+        <div className='signup-container'>
+            <h1>Hello from SignUp</h1>
+            <Link href="/">Head back home</Link>
+        </div>
+    );
+}
