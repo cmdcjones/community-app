479c1d6c6eb9981fa1a14150846e4ee0fafd1fd1
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-01 21:56:43 -0400

    update globals.css, app/page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link';
 
 export default function Home() {
-    return <h1>Hello, Vibrance</h1>;
+    return (
+        <>
+            <h1>Hello, Vibrance</h1>
+            <h2><Link href="/login">Log In</Link></h2>
+            <h2><Link href="/signup">Sign Up</Link></h2>
+        </>
+    );
 }
